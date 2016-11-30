96e8a36d4034e2b0f10e2c284701b32c1853a6a2
vladakg <vladimir.glisovic@hotmail.com>
2016-11-30 10:44:04 +0100

    REST controller was added

---
+++ application/controllers/Users.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Users extends REST_Controller {
+
+	public function test_get()
+	{
+		echo "sdsd";
+	}
+}
